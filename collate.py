3315ed007de9b0a72fd2f1c0833aad76f9939e14
Matt Brejza <mfb2g09@soton.ac.uk>
2014-01-06 16:34:50 +0000

    can collate more than folder at once

--- collate.py
+++ collate.py
@@ -16,8 +16,10 @@ import errno
 
 def print_usage():
 
-   print("Usage: \n\n")
-   print("\tcollate.py run_name -r results_folder -o output_folder \n\n")
+   print("\nUsage (single run): \n")
+   print("\tcollate.py run_name -r results_folder -o output_folder\n\n")
+   print("All folders in results path:\n")
+   print("\tcollate.py -a -r results_folder -o output_folder\n\n\n")
    
 def mkdir_p(path):
     try:
@@ -69,115 +71,127 @@ if ('o' not in args.keys()):
    print_usage()
    print(":(")
    exit()
-   
-run_name = sys.argv[1]
-res_path = args['r']+ "/" + run_name
-out_path = args['o']+ "/" + run_name
 
-out_suffix = "totals"
-
-print("Results folder: " + res_path)
-print("Run name to collate: " + run_name)
-
-
-file_list = glob.glob(res_path + "/*.json")
-
-print("Found files:")
-print(file_list)
+run_names = []
+if ('a' in args.keys()): 
+   cd = os.listdir(args['r'])
+   for i in cd:
+      if os.path.isdir(args['r']+ "/" + i):
+         print("Found directory: " + i)
+         run_names.append(i)
+else:
+   run_names.append(sys.argv[1])
 
 
-out_list = list();
+for run_name in run_names:
+
+   res_path = args['r']+ "/" + run_name
+   out_path = args['o']+ "/" + run_name
+
+   out_suffix = "totals"
+
+   print("Results folder: " + res_path)
+   print("Run name to collate: " + run_name)
 
 
-while len(file_list) > 0:
+   file_list = glob.glob(res_path + "/*.json")
 
-   #loop through each file
-   file = file_list.pop()
-   if os.path.basename(file).find(out_suffix) >= 0:
-      break
-   
-   #load the json
-   data = json.load(open(file))
+   print("Found files:")
+   print(file_list)
 
-   print("found: " + repr(len(data)) + " items")
-   
-   #check and get the first entry
-   if len(data) < 1:
-      break
-   if type(data) is dict:
-      item = data
-      data = ""
-   else:
-      item = data.pop()
+
+   out_list = list();
+
+
+   while len(file_list) > 0:
+
+      #loop through each file
+      file = file_list.pop()
+      if os.path.basename(file).find(out_suffix) >= 0:
+         break
       
+      #load the json
+      data = json.load(open(file))
 
-   #get data from the entry and merge into what we already have
-   while True:
+      print("found: " + repr(len(data)) + " items")
       
-      #if ('run_name' in item.keys()):
-      #   
-      if ('snr' in item.keys()):
-         #mush everything together
-         
-         #search through the list looking for a close match
-         found = -1
-         i=0
-         for s_d in out_list:
-            #look here
-            this_snr = s_d['snr']
-            if abs(this_snr - item['snr']) < 0.0005:
-               found = i
-               break
-            i=i+1
-                     
-            
-         if found < 0:
-            #add new dictionary
-            out_list.append(item)
-         else:
-            #merge
-            #go through existing list and add new data
-            for k in out_list[found].keys():
-               #GOT HERE DSRGERGRTGEHEH
-               if k == "total_bits":
-                  (out_list[found])[k] = (out_list[found])[k] + item[k]
-               elif k == "total_bit_errors":
-                  (out_list[found])[k] = (out_list[found])[k] + item[k]
-               elif k == "total_frames":
-                  (out_list[found])[k] = (out_list[found])[k] + item[k]
-               elif k == "total_frame_errors":
-                  (out_list[found])[k] = (out_list[found])[k] + item[k]
-               elif k == "total_symbols":
-                  (out_list[found])[k] = (out_list[found])[k] + item[k]
-               elif k == "total_symbol_errors":
-                  (out_list[found])[k] = (out_list[found])[k] + item[k]
-               elif k == "combined_complexity":
-                  if (out_list[found])["combined_complexity_step"] == item["combined_complexity_step"]:
-                     l1 = (out_list[found])[k]
-                     l2 = item[k]
-                     for a in range(0, min(len(l1), len(l2))):
-                        l1[a] = l1[a] + l2[a]
-                        
-                     if len(l2) > len(l1):
-                        l1.extend(l2[len(l1):])
-
-                     
-                  else:
-                     print("not yet implemented")
-                  
-      
-      
+      #check and get the first entry
       if len(data) < 1:
          break
-      item = data.pop()
+      if type(data) is dict:
+         item = data
+         data = ""
+      else:
+         item = data.pop()
+         
 
-print("\n\n\n\n\n")
-print(out_list)
+      #get data from the entry and merge into what we already have
+      while True:
+         
+         #if ('run_name' in item.keys()):
+         #   
+         if ('snr' in item.keys()):
+            #mush everything together
+            
+            #search through the list looking for a close match
+            found = -1
+            i=0
+            for s_d in out_list:
+               #look here
+               this_snr = s_d['snr']
+               if abs(this_snr - item['snr']) < 0.0005:
+                  found = i
+                  break
+               i=i+1
+                        
+               
+            if found < 0:
+               #add new dictionary
+               out_list.append(item)
+            else:
+               #merge
+               #go through existing list and add new data
+               for k in out_list[found].keys():
+                  #GOT HERE DSRGERGRTGEHEH
+                  if k == "total_bits":
+                     (out_list[found])[k] = (out_list[found])[k] + item[k]
+                  elif k == "total_bit_errors":
+                     (out_list[found])[k] = (out_list[found])[k] + item[k]
+                  elif k == "total_frames":
+                     (out_list[found])[k] = (out_list[found])[k] + item[k]
+                  elif k == "total_frame_errors":
+                     (out_list[found])[k] = (out_list[found])[k] + item[k]
+                  elif k == "total_symbols":
+                     (out_list[found])[k] = (out_list[found])[k] + item[k]
+                  elif k == "total_symbol_errors":
+                     (out_list[found])[k] = (out_list[found])[k] + item[k]
+                  elif k == "combined_complexity":
+                     if (out_list[found])["combined_complexity_step"] == item["combined_complexity_step"]:
+                        l1 = (out_list[found])[k]
+                        l2 = item[k]
+                        for a in range(0, min(len(l1), len(l2))):
+                           l1[a] = l1[a] + l2[a]
+                           
+                        if len(l2) > len(l1):
+                           l1.extend(l2[len(l1):])
 
-mkdir_p(out_path)
-f = open(out_path+"/results.json",'w')
-f.write(json.dumps(out_list))
-f.close()
+                        
+                     else:
+                        print("not yet implemented")
+                     
+         
+         
+         if len(data) < 1:
+            break
+         item = data.pop()
+
+   print("\n\n\n\n\n")
+   print(out_list)
+
+   mkdir_p(out_path)
+   f = open(out_path+"/results.json",'w')
+   f.write(json.dumps(out_list))
+   f.close()
 
 
 
