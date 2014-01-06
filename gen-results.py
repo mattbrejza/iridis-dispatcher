a515f131b1f4bc01f61df560f13eda6a3bdd6635
Matt Brejza <mfb2g09@soton.ac.uk>
2014-01-06 15:49:36 +0000

    adding scp copying

--- gen-results.py
+++ gen-results.py
@@ -157,3 +157,6 @@ if ('snr' in res[0].keys()) and ('total_symbols' in res[0].keys()) and ('total_s
    f.close()
    #run gnuplot
    system("gnuplot "+args['o']+ "/" + run_name + "/ser_gnuplot.gp")
+   
+system("rsync -rave ssh " + args['o']+ "/" + run_name +"/*.png ftp:public_html/results/" run_name)
+system("ssh ftp \"chmod go+r ~/public_html/results -R\"")
