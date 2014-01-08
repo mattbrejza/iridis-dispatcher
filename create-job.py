5883e695ab36f6fec972ab2b54f8e1869bc2d63b
Matt Brejza <mfb2g09@soton.ac.uk>
2014-01-08 17:24:37 +0000

    added uec impl jobs

--- create-job.py
+++ create-job.py
@@ -177,6 +177,127 @@ def uecadap(file, args):
    
    f.close()  
    
+def uecimpl(file, args):   
+   
+   print("creating job file for uec implementation job reference code")
+
+   
+   #look for options like -startsnr, -stopsnr etc
+   opt_dict = getoptions(args)
+   
+   die=0
+   
+   if ('snrs' not in opt_dict.keys()):   
+      if ('startsnr' not in opt_dict.keys()):
+         die=1
+         print("Requires option -startsnr")     
+      if ('stopsnr' not in opt_dict.keys()):
+         die=1
+         print("Requires option -stopsnr")      
+      if ('stepsnr' not in opt_dict.keys()):
+         die=1
+         print("Requires option -stepsnr")
+      
+      if die==0:
+         startsnr = float(opt_dict['startsnr'])
+         stopsnr = float(opt_dict['stopsnr'])
+         stepsnr = float(opt_dict['stepsnr'])
+         snrs = numpy.arange(startsnr, stopsnr+0.0002, stepsnr)
+         print("List of SNRs:")
+         print (snrs)
+      
+   else:
+      snrs_s = opt_dict['snrs']
+      snrs_s=snrs_s.split(",")
+      snrs = [float(s) for s in snrs_s]
+      print("List of SNRs:")
+      print (snrs)
+         
+   
+   print ("\n\n")
+   
+   if ('copies' not in opt_dict.keys()):
+      copies = 1;
+      print("Defaulting -copies to 1")
+   else:
+      copies = int(opt_dict['copies'])
+      
+   if ('wall' not in opt_dict.keys()):
+      wall = 36;
+      print("Defaulting -wall to 36")
+   else:
+      wall = opt_dict['wall'] 
+      
+   if ('n' not in opt_dict.keys()):
+      name = time.strftime("%y%m%d-%H%M%S");
+      print("Defaulting -n (name) to "+name)
+   else:
+      name = opt_dict['n'] 
+
+   if ('bits' not in opt_dict.keys()):
+      bits = 1024;
+      print("Defaulting -bits to 1024")
+   else:
+      bits = float(opt_dict['bits'])   
+
+#   if ('type' not in opt_dict.keys()):
+#      die=1
+#      print("Requires option -type. Choose either ET,EUT,UT2")
+
+#   if ('d' not in opt_dict.keys()):
+#      demod = 1;
+#      print("Defaulting -d (demod) to 1")
+#   else:
+#      demod = opt_dict['d'] 
+
+      
+   if ('src' not in opt_dict.keys()):
+      die=1
+      print("Requires option -src")
+      
+   if ('res' not in opt_dict.keys()):
+      die=1
+      print("Requires option -res")
+      
+      
+   if die>0:
+      print("Quiting...")
+      return
+
+   #total nodes
+   nodes = len(snrs)
+   if nodes < 1 or copies < 1:
+      print("Invalid combination of snr start/stop/step / copies")
+      print("Quiting...")
+      return
+   
+   nodes = nodes * copies
+   
+   mach = math.ceil(nodes/float(16))
+   ppn = math.ceil(nodes/mach)
+   
+   #open file for writing
+   f = open(file,'w')
+   write_preamble(f,mach,ppn,wall)
+   f.write("SRC=\""+opt_dict['src']+"\"\n")
+   f.write("RES=$PBS_O_WORKDIR/\""+opt_dict['res']+"/"+name+"/\"\n")
+   f.write("mkdir -p $RES\n\n\n\n")
+   f.write("cd $SRC\n")
+   f.write("mcc -vm run_exit_ber.m *.m\n")
+   f.write("cd $PBS_O_WORKDIR\n")
+   f.write("COMMON=\"modulation qpsk start_snr 1.6 stop_snr 3 step_snr 0.2 pivi 0 int1 srandn int2 srandn stopping_mi 2 minimum_tx_bits 1000000000 enable_trellis_block 1 trellis_knows_first 1 use_trellis_iter 1\"")
+   
+   
+   for c in range(0, copies):
+      for snr in snrs:
+      #   f.write("matlab -nodisplay -nojvm -r \"cd $SRC; "+cmmd+"  "+repr(snr)+" "+repr(int(symbols))+" 1 '$RES'\"&\n")
+         f.write("./$SRC/run_run_exit_ber.sh /home/local/software/rh53/matlab/2011a $COMMON \"seed\" 41 \"results_filename\" \"$RES\" \"start_snr "+repr(snr)+" stop_snr "+repr(snr)+" step_snr 0.2\" \"generate_unary_mode\" 3 \"reuse_demod\" 1 \"qpsk_mapping\" \"n\" \"decoding_loops\" 10 \"int3_1\" \"same\" \"int3\" \"randn\"  \"number_type\" \"do\" &")
+      f.write("\n")
+      
+   f.write("\nwait\n")
+   
+   f.close()  
+   
 def uecadapref(file, args):
 
    print("creating job file for uec adaptive job reference code")
@@ -306,6 +427,7 @@ try:
 
    options = { "uec-adaptive" : uecadap,
                "uec-adaptive-ref" : uecadapref,
+               "uec-impl" : uecimpl,
              }
              
    options[sys.argv[2]](sys.argv[1],sys.argv[3:])
@@ -313,4 +435,4 @@ try:
 except IndexError:
    print("Usage: \n\n")
    print("\tcreate-job.py job_out run_type <specific options>\n\n")
-   print("\trun_type options: \n\t\tuec-adaptive\tuec-adaptive-ref\n")
+   print("\trun_type options: \n\t\tuec-adaptive\tuec-adaptive-ref\tuec-impl\n")
